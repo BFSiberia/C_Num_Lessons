f3564cfc67beec2e10a8e614bfe83e35ce026eec
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-13 11:21:08 +0800

    add Example003 Random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,7 @@
 ﻿int a, b, sum;
-a = 3;
-b = 5;
+a = new Random().Next(1,10); // Случайное число от 1 до 9
+b = new Random().Next(1,10);
+Console.WriteLine($"Число a равно {a}, число b равно {b}.");
 sum = a+b;
-Console.WriteLine(sum);
+Console.WriteLine($"Сумма чисел равна {sum}");
 
