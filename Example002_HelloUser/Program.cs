1475ad170f141aa15ba5e7b4c56164c7087f6d95
BFSiberia <97689999+BFSiberia@users.noreply.github.com>
2022-04-13 10:54:16 +0800

    Update Program.cs
    Edit lines

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите имя пользователя: ");
+﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
-Console.WriteLine(username);
+Console.Write(username);
