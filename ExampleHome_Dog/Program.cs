6bb97ee8da0d1347fd620afe56f8e2d6038fcbfc
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-13 12:50:10 +0800

    add ExampleHome_Dog

---
+++ ExampleHome_Dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int distance = 10000;
+int friendSpeed1 = 1;
+int friendSpeed2 = 2;
+int dogSpeed = 5;
+int time = 0;
+int friend = 2;
+
+while (distance >10)
+{
+    if (friend == 1)
+    {
+        time = distance/(friendSpeed1 + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(friendSpeed2 + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (friendSpeed1 + friendSpeed2) * time;
+    count++;
+}
+
+Console.WriteLine($"Собака пробежала {count} раз.");
