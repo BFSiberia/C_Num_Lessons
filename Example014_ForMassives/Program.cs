214c2b21f067d693c87653cbc7584a87b3956873
Ольга Смолина <a.v.tarasov@gmail.com>
2022-04-27 11:52:17 +0800

    add Example015_Recursion

--- Example014_ForMassives/Program.cs
+++ Example014_ForMassives/Program.cs
@@ -1,11 +1,11 @@
 ﻿//Упорядочивание массивов
 
-int[] arr = {1,5,4,3,2,6,7,1,1};
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
 void PrintArray(int[] array)
 {
     int count = array.Length;
-    for(int i = 0; i<count; i++ )
+    for (int i = 0; i < count; i++)
     {
         Console.Write($"{array[i]} ");
     }
@@ -14,13 +14,13 @@ void PrintArray(int[] array)
 
 void SelectionSort(int[] array)
 {
-    for(int i =0; i<array.Length-1; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
 
-        for(int j = i+1; j<array.Length; j++)
+        for (int j = i + 1; j < array.Length; j++)
         {
-            if(array[j]< array[minPosition]) minPosition = j;
+            if (array[j] < array[minPosition]) minPosition = j;
         }
 
         int temporary = array[i];
